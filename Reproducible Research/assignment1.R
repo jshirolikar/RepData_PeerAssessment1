655760af6b5086fc70c99d28e7a94b0bff65d2a0
jshirolikar <jshirolikar@yahoo.com>
2015-01-17 23:54:11 -0800

    Assignment 1

--- Reproducible Research/assignment1.R
+++ Reproducible Research/assignment1.R
@@ -1,14 +1,14 @@
-require("xlsx")
+library("stats")
 library("xlsx")
 library("plyr")
 library("stringr")
 library("dplyr")
 
 activitydocorig <- read.csv2("C:\\coursera\\coursera\\Reproducible Research\\activity.csv", na.string = "", sep = ",")
-activitydoc <- activitydocorig[complete.cases(activitydoc),]
+activitydoc <- subset(activitydocorig, activitydocorig$steps != "NA")
 
+missingactivitydoc <- subset(activitydocorig, activitydocorig$steps == "NA")
 
-activitydoc <- activitydoc[289:17568,]
 
 activitydoc$steps <- as.numeric(activitydoc$steps)
 activitydoc$date <- as.Date(activitydoc$date)
@@ -21,58 +21,58 @@ print(mean(sum_activitydoc$x))
 print(median(sum_activitydoc$x))
 
 average_dailyactivitydoc <- aggregate(activitydoc$steps, list(activitydoc$interval), mean)
-plot(x=average_dailyactivitydoc$x,y= average_dailyactivitydoc$Group1,   type = "l", xlab = "Interval", ylab = "Steps", xaxt = "n", yaxt = "n")
-axis(1, at=c(average_dailyactivitydoc$Group1))
+plot(y=average_dailyactivitydoc$x, x=average_dailyactivitydoc$Group.1,   type = "l", xlab = "Interval", ylab = "Steps", xaxt = "n", yaxt = "n")
+axis(1, at=c(average_dailyactivitydoc$Group.1))
 axis(2, at = c(average_dailyactivitydoc$x))
 
-#print interval with max steps
+#print interval with max steps 
 average_dailyactivitydoc$Group.1[which(average_dailyactivitydoc$x == max(average_dailyactivitydoc$x))]
 
 
-newactivitydoc <- activitydocorig[1:288,]
-newactivitydoc$steps <- as.numeric(newactivitydoc$steps)
-newactivitydoc$date <- as.Date(newactivitydoc$date)
-newactivitydoc$steps[1:288] <- average_dailyactivitydoc$x[1:288]
 
-newactivitydoc <- rbind(newactivitydoc, activitydoc)
+
+missingactivitydoc$steps <- as.numeric(missingactivitydoc$steps)
+missingactivitydoc$interval <- as.numeric(missingactivitydoc$interval)
+
+j = 0
+
+for (j in 0:7)
+{
+    missingactivitydoc$steps[((j*288) + 1) : ((j+1)*288)] <- average_dailyactivitydoc$x[1:288]
+    
+ 
+}
+
+
+newactivitydoc <- rbind(activitydoc, missingactivitydoc)
+newactivitydoc$steps <- as.numeric(newactivitydoc$steps)
+newactivitydoc$interval <- as.numeric(newactivitydoc$interval)
 
 sum_activitydoc <- aggregate(newactivitydoc$steps, list(newactivitydoc$date), sum)
-hist(sum_activitydoc$x, xlab = "Total steps per day")
+hist(sum_activitydoc$x, xlab = "Total steps per day", main = "Histogram of total steps per day")
 print(mean(sum_activitydoc$x))
 print(median(sum_activitydoc$x))
 
 
 newactivitydoc$weekday <- weekdays(newactivitydoc$date)
+newactivitydoc2 <- newactivitydoc
 newactivitydoc <- subset(newactivitydoc, newactivitydoc$weekday == "Sunday" | newactivitydoc$weekday== "Saturday")
 
-average_weekendactivitydoc <- aggregate(newactivitydoc$steps, list(newactivitydoc$interval), mean)
+average_weekendactivitydoc <- aggregate((newactivitydoc$steps), list(newactivitydoc$interval), mean)
 
-plot(x=average_weekendactivitydoc$x,y= average_weekendactivitydoc$Group1,   type = "l", xlab = "Interval", ylab = "Steps", xaxt = "n", yaxt = "n")
-axis(1, at=c(average_weekendactivitydoc$Group1))
+plot(y=average_weekendactivitydoc$x,x= average_weekendactivitydoc$Group.1,   type = "l", xlab = "Interval", ylab = "Steps", xaxt = "n", yaxt = "n")
+axis(1, at=c(average_weekendactivitydoc$Group.1))
 axis(2, at = c(average_weekendactivitydoc$x))
 
 
 #weekdays
-newactivitydoc <- activitydocorig[1:288,]
-newactivitydoc$steps <- as.numeric(newactivitydoc$steps)
-newactivitydoc$date <- as.Date(newactivitydoc$date)
-newactivitydoc$steps[1:288] <- average_dailyactivitydoc$x[1:288]
-
-newactivitydoc <- rbind(newactivitydoc, activitydoc)
-
-sum_activitydoc <- aggregate(newactivitydoc$steps, list(newactivitydoc$date), sum)
-hist(sum_activitydoc$x, xlab = "Total steps per day")
-print(mean(sum_activitydoc$x))
-print(median(sum_activitydoc$x))
-
-
-newactivitydoc$weekday <- weekdays(newactivitydoc$date)
+newactivitydoc <- newactivitydoc2
 newactivitydoc <- subset(newactivitydoc, newactivitydoc$weekday != "Sunday" & newactivitydoc$weekday != "Saturday")
 
 average_weekdayactivitydoc <- aggregate(newactivitydoc$steps, list(newactivitydoc$interval), mean)
 
-plot(x=average_weekdayactivitydoc$x,y= average_weekdayactivitydoc$Group1,   type = "l", xlab = "Interval", ylab = "Steps", xaxt = "n", yaxt = "n")
-axis(1, at=c(average_weekdayactivitydoc$Group1))
+plot(y=average_weekdayactivitydoc$x,x= average_weekdayactivitydoc$Group.1,   type = "l", xlab = "Interval", ylab = "Steps", xaxt = "n", yaxt = "n")
+axis(1, at=c(average_weekdayactivitydoc$Group.1))
 axis(2, at = c(average_weekdayactivitydoc$x))
 
 
